991f369a5d740925c355a30f4e6a58e9bfb6ca5b
Reto Wettstein <retowettstein@hotmail.com>
2015-03-02 17:48:02 +0100

    Exercise 2 some code formatting

--- src/net/tomp2p/exercise/Exercise2.java
+++ src/net/tomp2p/exercise/Exercise2.java
@@ -16,34 +16,35 @@ import net.tomp2p.peers.Number640;
 import net.tomp2p.peers.PeerAddress;
 import net.tomp2p.storage.Data;
 
+
 public class Exercise2 {
-    
-    public static final int NUMBER_OF_PEERS = 10;
-    public static final int STORING_PEER_INDEX = 2; // peerIndex is 1 smaller than peerId: peerIndex 0 is peerId 1
-    public static final int GETTER_PEER_INDEX = 4; // peerIndex is 1 smaller than peerId: peerIndex 0 is peerId 1
-    public static final Number160 KEY = new Number160(12345);
-    public static final int PORT = 4001;
-    
+
+    public static final int       NUMBER_OF_PEERS    = 10;
+    public static final int       STORING_PEER_INDEX = 2; // peerIndex is 1 smaller than peerId: peerIndex 0 is peerId 1
+    public static final int       GETTER_PEER_INDEX  = 4; // peerIndex is 1 smaller than peerId: peerIndex 0 is peerId 1
+    public static final Number160 KEY                = new Number160(12345);
+    public static final int       PORT               = 4001;
+
     public static void main(String[] args) {
         PeerDHT[] peers = null;
-        
+
         try {
             peers = createAndAttachPeersDHT(NUMBER_OF_PEERS, PORT);
             bootstrap(peers);
-            
+
             put(peers[STORING_PEER_INDEX], KEY, "Max Power");
             get(peers[GETTER_PEER_INDEX], KEY);
-            
+
             peersShutdown(peers);
         } catch (IOException pEx) {
             pEx.printStackTrace();
         } catch (ClassNotFoundException pEx) {
             pEx.printStackTrace();
         }
-        
+
     }
-    
-    
+
+
     /**
      * Create peers with a port and attach it to the first peer in the array.
      * 
@@ -52,34 +53,35 @@ public class Exercise2 {
      * @return The created peers
      * @throws IOException IOException
      */
-    public static PeerDHT[] createAndAttachPeersDHT(int nr, int port) throws IOException{
+    public static PeerDHT[] createAndAttachPeersDHT(int nr, int port)
+            throws IOException {
         PeerDHT[] peers = new PeerDHT[nr];
-        for (int i = 0; i < nr; i++){
-            if (i == 0){
-                peers[0] = new PeerBuilderDHT(new PeerBuilder(new Number160(i+1)).ports(port).start()).start();
+        for (int i = 0; i < nr; i++) {
+            if (i == 0) {
+                peers[0] = new PeerBuilderDHT(new PeerBuilder(new Number160(i + 1)).ports(port).start()).start();
             } else {
-                peers[i] = new PeerBuilderDHT(new PeerBuilder(new Number160(i+1)).masterPeer(peers[0].peer()).start()).start();
+                peers[i] = new PeerBuilderDHT(new PeerBuilder(new Number160(i + 1)).masterPeer(peers[0].peer()).start()).start();
             }
         }
-        
+
         return peers;
     }
-    
-    
+
+
     /**
      * Bootstraps peers to the first peer in the array.
      * 
      * @param peers The peers that should be bootstrapped
      */
     public static void bootstrap(PeerDHT[] peers) {
-        //make perfect bootstrap, the regular can take a while
-        for(int i=0;i<peers.length;i++) {
-            for(int j=0;j<peers.length;j++) {
+        // make perfect bootstrap, the regular can take a while
+        for (int i = 0; i < peers.length; i++) {
+            for (int j = 0; j < peers.length; j++) {
                 peers[i].peerBean().peerMap().peerFound(peers[j].peerAddress(), null, null, null);
             }
         }
     }
-    
+
     /**
      * Put data into the DHT.
      * 
@@ -88,13 +90,14 @@ public class Exercise2 {
      * @param pValue The data to be stored
      * @throws IOException IOException
      */
-    public static void put(PeerDHT pPeer, Number160 pKey, String pValue) throws IOException{
+    public static void put(PeerDHT pPeer, Number160 pKey, String pValue)
+            throws IOException {
         FuturePut futurePut = pPeer.put(pKey).data(new Data(pValue)).start();
         futurePut.awaitUninterruptibly();
-        
+
         System.out.println("Peer with id " + pPeer.peerAddress().peerId().intValue() + " stored " + "[Key: " + pKey.intValue() + ", Value: " + pValue + "]");
     }
-    
+
     /**
      * Put data into the DHT.
      * 
@@ -104,35 +107,36 @@ public class Exercise2 {
      * @throws IOException IOException
      * @throws ClassNotFoundException ClassNotFoundException.
      */
-    public static Object get(PeerDHT pPeer, Number160 pKey) throws ClassNotFoundException, IOException{
+    public static Object get(PeerDHT pPeer, Number160 pKey)
+            throws ClassNotFoundException, IOException {
         Object returnValue;
-        
+
         FutureGet futureGet = pPeer.get(pKey).start();
         futureGet.awaitUninterruptibly();
-        
+
         Set<Entry<PeerAddress, Map<Number640, Data>>> replies = futureGet.rawData().entrySet();
-        
+
         returnValue = futureGet.data().object();
         System.out.println("\nPeer with id " + pPeer.peerAddress().peerId().intValue() + " received for key " + pKey.intValue() + " the data: " + returnValue);
-        
-        System.out.println("\nThe peers with the following id's replied:");
+
+        System.out.print("\nThe peers with the following id's replied: ");
         Iterator<Entry<PeerAddress, Map<Number640, Data>>> iter = replies.iterator();
-        while(iter.hasNext()){
+        while (iter.hasNext()) {
             Entry<PeerAddress, Map<Number640, Data>> entry = iter.next();
-            System.out.println(entry.getKey().peerId().intValue());
+            System.out.print(entry.getKey().peerId().intValue() + " ");
         }
-        
+
         return returnValue;
     }
-    
-    
+
+
     /**
      * Shutdown peers.
      * 
      * @param pPeers The peers that should be shutdown
      */
-    public static void peersShutdown(PeerDHT[] pPeers){
-        for(int i = 0; i < pPeers.length; i++){
+    public static void peersShutdown(PeerDHT[] pPeers) {
+        for (int i = 0; i < pPeers.length; i++) {
             pPeers[i].shutdown();
         }
     }
