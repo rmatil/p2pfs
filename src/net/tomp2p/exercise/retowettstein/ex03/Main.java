96723adb0b86ac851265d4ffbeeccf8da1405b6e
Reto Wettstein <retowettstein@hotmail.com>
2015-03-06 19:37:06 +0100

    Added recommended folder structure for ex03

---
+++ src/net/tomp2p/exercise/retowettstein/ex03/Main.java
@@ -0,0 +1,12 @@
+package net.tomp2p.exercise.retowettstein.ex03;
+
+/**
+ * @author Reto Wettstein 12-716-221
+ * @author Christian Tresch 06-923-627
+ */
+public class Main {
+
+    public static void main(String[] args){
+        
+    }
+}
