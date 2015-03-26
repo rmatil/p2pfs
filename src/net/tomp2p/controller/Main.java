e9faa8a65e11ea47b049e1282a0753ed8e0be373
Reto Wettstein <retowettstein@hotmail.com>
2015-03-26 13:28:39 +0100

    commented out code for working over internet and added code to test on own machine

--- src/net/tomp2p/controller/Main.java
+++ src/net/tomp2p/controller/Main.java
@@ -12,44 +12,45 @@ import net.tomp2p.util.MyJSONParser;
 public class Main {
 
     public static void main(String[] args) {
-//        BoostrapServerAccess boostrapServerAccess = new BoostrapServerAccess();
+        BoostrapServerAccess boostrapServerAccess = new BoostrapServerAccess();
         FSPeer fsPeer = new FSPeer();
         
-//        // 2 ways to get the ip address
-//        String myIP = fsPeer.findLocalIp();
-//        // String myIP = fsPeer.findExternalIp();
-//        int myPort = 4000;
-//        
-//        List<Map<String, String>> ipList = MyJSONParser.parse(boostrapServerAccess.get());
-//        int size = ipList.size();
-//        
-//        boostrapServerAccess.post(myIP, 4000);
-//        
-        try {
-//            if(size == 0){   
-//                fsPeer.startAsBootstrapPeer(myIP, myPort);
-//            } else {
-//                boolean success = false;
-//                int counter = 0;
-//                
-//                while(!success || counter < size){
-//                  success = fsPeer.startPeer(myIP, ipList.get(0).get("address"), 4000, Integer.parseInt(ipList.get(0).get("port")));
-//                  counter++;
-//                }
-//                
-//                if(success){
-//                    System.out.println("Bootstrap succesfull");
-//                } else {
-//                    System.out.println("No connection possible");
-//                }
-//            }
+        // 2 ways to get the ip address
+        String myIP = fsPeer.findLocalIp();
+        // String myIP = fsPeer.findExternalIp();
+        int myPort = 4000;
         
-            String myIP = "192.168.0.17";
-            int myPort = 4000;
-//            fsPeer.startAsBootstrapPeer(myIP, myPort);
-            fsPeer.startPeer(myIP, myIP, myPort, myPort);
-            InputReader.readAndProcess(fsPeer);
-                       
+        List<Map<String, String>> ipList = MyJSONParser.parse(boostrapServerAccess.get());
+        int size = ipList.size();
+        
+        boostrapServerAccess.post(myIP, 4000);
+       
+        try {
+            if(size == 0){   
+                fsPeer.startAsBootstrapPeer(myIP, myPort);
+            } else {
+                boolean success = false;
+                int counter = 0;
+                
+                while(!success || counter < size){
+                  success = fsPeer.startPeer(myIP, ipList.get(0).get("address"), 4000, Integer.parseInt(ipList.get(0).get("port")));
+                  counter++;
+                }
+                
+                if(success){
+                    System.out.println("Bootstrap succesfull");
+                } else {
+                    System.out.println("No connection possible");
+                }
+            }
+        
+//            // for testing on own pc
+//            String myIP = "172.20.10.5";
+//            int myPort = 4000;
+//            // fsPeer.startAsBootstrapPeer(myIP, myPort);
+//            fsPeer.startPeer(myIP, myIP, myPort, myPort);
+            
+            InputReader.readAndProcess(fsPeer);         
             fsPeer.shutdown();
         } catch (Exception pEx) {
             pEx.printStackTrace();
