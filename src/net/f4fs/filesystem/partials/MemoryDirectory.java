77b5bde01e163a5285e35b4a8ebed4811240f93a
Raphael Matile <raphael.matile@bluewin.ch>
2015-04-16 19:36:59 +0200

    Set attributes on file and directories

--- src/net/f4fs/filesystem/partials/MemoryDirectory.java
+++ src/net/f4fs/filesystem/partials/MemoryDirectory.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import net.f4fs.config.FSStatConfig;
 import net.f4fs.fspeer.FSPeer;
 import net.fusejna.DirectoryFiller;
 import net.fusejna.StructStat.StatWrapper;
@@ -60,7 +61,47 @@ public class MemoryDirectory
 
     @Override
     public void getattr(final StatWrapper stat) {
-        stat.setMode(NodeType.DIRECTORY);
+        long currentUnixTimestamp = System.currentTimeMillis() / 1000l;
+
+        // time of last access
+        stat.atime(currentUnixTimestamp);
+        // time of last data modification
+        stat.mtime(currentUnixTimestamp);
+        // Time when file status was last changed (inode data modification).
+        // Changed by the chmod(2), chown(2), link(2), mknod(2), rename(2), unlink(2), utimes(2) and write(2) system calls.
+        stat.ctime(currentUnixTimestamp);
+        
+        // ID of device containing file
+        // stat.dev(dev);
+
+        // file generation number
+        // stat.gen(gen);
+
+        // Group ID of the file
+        // stat.gid(gid);
+
+        // Note: if ino and rdev are taken together, they uniquely 
+        // identify the file among multiple filesystems
+        // File serial number
+        // stat.ino(ino); // only unique on the current FS
+        // Device ID
+        // stat.rdev(rdev); 
+                
+        // Number of hard links which link to this file
+        // Hard links are multiple directory entries which link to the same file -> created by link system call.
+        // From man link: "A hard link to a file is indistinguishable from the original directory entry; any changes to a file are effectively inde-
+        // pendent of the name used to reference the file.  Hard links may not normally refer to directories and may not span file systems."
+        // TODO: how do we check these?
+        // stat.nlink(0);
+        
+        // set access modes
+        stat.setMode(NodeType.DIRECTORY, true, true, true, true, true, true, true, true, true);
+        
+        // NOTE: according to the manual entry of man 2 stat these fields should not be changed 
+        // RESERVED: DO NOT USE!
+        // stat.lspare(lspare);
+        // RESERVED: DO NOT USE!
+        // stat.qspare(qspare);
     }
 
     public synchronized void mkdir(final String lastComponent) {
