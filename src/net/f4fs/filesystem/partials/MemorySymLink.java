c205bf05eae1b97b593e39e47ab236794437ad10
Raphael Matile <raphael.matile@bluewin.ch>
2015-05-02 19:21:54 +0200

    Fix symbolic links creation

--- src/net/f4fs/filesystem/partials/MemorySymLink.java
+++ src/net/f4fs/filesystem/partials/MemorySymLink.java
@@ -1,16 +1,11 @@
 package net.f4fs.filesystem.partials;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.logging.Logger;
 
 import net.f4fs.fspeer.FSPeer;
 import net.fusejna.StructStat.StatWrapper;
 import net.fusejna.types.TypeMode.NodeType;
-import net.tomp2p.dht.FuturePut;
-import net.tomp2p.dht.FutureRemove;
-import net.tomp2p.peers.Number160;
-import net.tomp2p.storage.Data;
 
 
 public class MemorySymLink
@@ -25,21 +20,6 @@ public class MemorySymLink
      */
     private ByteBuffer  contents = ByteBuffer.allocate(0);
 
-    /**
-     * A symbolic link <code>target</code> is created to <code>path</code>.
-     * (<code>target</code> is the name of the file created, <code>path</code> is the string used in creating the symbolic link)
-     * 
-     * @param path The already existing file to which the link should be created
-     * @param target The path of the symlink which points to <code>path</code>
-     * @param peer The FSPeer
-     */
-    public MemorySymLink(final AMemoryPath path, final String target, final FSPeer peer) {
-        super(target, peer);
-        this.existingPath = path;
-        this.target = target;
-
-        this.init();
-    }
 
     /**
      * A symbolic link <code>target</code> is created to <code>path</code>.
@@ -54,29 +34,12 @@ public class MemorySymLink
         super(target, parent, peer);
         this.existingPath = path;
         this.target = target;
-
-        this.init();
-    }
-
-    private void init() {
-        // because AMemoryPath also stores data in the DHT of this symlink
-        // and we have to extend from it, we have to remove this here again...
-        // TODO: If anyone can think of a better idea, please change
-        try {
-            // a symbolic link must contain the name of the target as content
-            // as stated in <code>man ln</code>
-            contents = ByteBuffer.wrap(target.getBytes());
-            FuturePut futurePut = super.getPeer().putData(Number160.createHash(getPath()), new Data(contents.array()));
-            futurePut.await();
-
-            // create the symlink to the target
-            futurePut = super.getPeer().putPath(Number160.createHash(getPath()), new Data(this.existingPath.getPath()));
-            futurePut.await();
-
-            logger.info("Created symlink '" + target + "' on path '" + getPath() + "' to file on path '" + this.existingPath.getPath() + "'.");
-        } catch (InterruptedException | IOException e) {
-            logger.warning("Could not create symlink '" + target + "' on path '" + getPath() + "' to file on path '" + this.existingPath.getPath() + "'. Message: " + e.getMessage());
-        }
+        
+        // add the target's path as content as required
+        // and stated in <code>man ln</code>
+        contents = ByteBuffer.wrap(target.getBytes());
+        
+        logger.info("Created symlink '" + target + "' on path '" + getPath() + "' to file on path '" + existingPath.getPath() + "'.");
     }
 
     @Override
